0e56c587160de54013efa7db55a7aa20fd9fadcf
Tharki-God <alokender018@gmail.com>
2024-04-21 08:23:12 +0530

    meow

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import Settings from "./Components/Settings";
 
 export const start = (): void => {
   Settings.registerSettings();
-  Injections.applyInjections();
+  void Injections.applyInjections();
 };
 
 export const stop = (): void => {
